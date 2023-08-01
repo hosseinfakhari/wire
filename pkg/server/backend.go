ba132f66876c44715f5d4f78dae4bc86bc546efb
Hossein Fakhari <fakhari.hossein@gmail.com>
2023-08-01 17:17:33 +0100

    struct for backend and round-robin traverse

---
+++ pkg/server/backend.go
@@ -0,0 +1,21 @@
+package server
+
+import (
+	"net/http/httputil"
+	"net/url"
+	"sync"
+)
+
+type Backend struct {
+	URL          *url.URL
+	Alive        bool
+	mux          sync.RWMutex
+	ReverseProxy *httputil.ReverseProxy
+}
+
+func (b *Backend) IsAlive() (alive bool) {
+	b.mux.RLock()
+	alive = b.Alive
+	b.mux.RUnlock()
+	return
+}
